1bc1a41a26b626a63a5f99da53f178305d71a8a1
Michel Fisher <mich013@hotmail.com>
2015-09-02 12:09:46 -0400

    added site.py and updated groovy file

---
+++ site.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# this holds a dictionary site that contains the labels of the devices used arranged by both device and room
+
+site = {
+          "devices": [
+                        "Aeon Ss 01",
+                        "Aeon Ss 02",
+                        "Aeon SmartStrip 03",
+                        "SmartSense Motion/Temp Sensor 01"
+                     ],
+          "rooms":   {
+                        "Michels Office" : [
+                                              "Aeon Ss 01",
+                                              "Aeon Ss 02",
+                                              "Aeon SmartStrip 03",
+                                              "SmartSense Motion/Temp Sensor 01"
+                                           ]
+                      }
+       }
